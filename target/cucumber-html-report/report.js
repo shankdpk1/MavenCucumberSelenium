62c1e3d9722115b5c177315f1286a4f5e4cb1631
shashank <shashank.deepak@gmail.com>
2023-02-28 20:33:47 -0600

    hi

--- target/cucumber-html-report/report.js
+++ target/cucumber-html-report/report.js
@@ -23,7 +23,7 @@ formatter.match({
   "location": "Steps.printMsg()"
 });
 formatter.result({
-  "duration": 115107500,
+  "duration": 103287500,
   "status": "passed"
 });
 formatter.scenario({
@@ -49,7 +49,7 @@ formatter.match({
   "location": "Steps.printMsg()"
 });
 formatter.result({
-  "duration": 64700,
+  "duration": 555300,
   "status": "passed"
 });
 });
